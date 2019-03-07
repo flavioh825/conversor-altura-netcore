6f9fc2b2edbd33eedf32a6780f3bf6ffa30d3d11
Flavio Henrique <flavioh825@gmail.com>
2019-03-07 16:35:38 -0300

    Conversor funcionando corretamente.

---
+++ APIAlturas/Controllers/ConversorAlturasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Alturas;
+
+namespace APIAlturas.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConversorAlturasController : Controller
+    {
+        // GET api/values
+        [HttpGet("PesMetros/{pes}")]
+        public object Get(double pes)
+        {
+            return new
+                    {
+                        alturasPes = pes,
+                        alturasMetros = ConversorAlturas.PesParaMetros(pes)
+                    };
+        }
+    }
+}
